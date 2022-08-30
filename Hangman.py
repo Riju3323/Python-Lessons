ef1f306359a8939194c35b0dd7d7725974e80f47
Riju <87523667+Riju3323@users.noreply.github.com>
2022-08-30 20:45:07 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,36 @@
-word_list = ["ardvark","baboon","camel"]
+import random
+wordd_list = ["ardvark","baboon","camel"]
+numrnndm = (random.randint(0,2))
+wrddd = wordd_list[numrnndm]
+compl = False
+display = []
+numchk = len(wrddd)
+ercnt = 3
 
-print(word_list)
+for i in wrddd:
+  display += "_"
+
+print(display)
+
+while compl:
+  letter = input("Guess a Letter: ").lower()
+  for i in range(len(wrddd)):
+    if letter == wrddd[i]:
+      display[i]=letter
+      numchk -= 1
+    else:
+      ercnt -= 1
+      print("Chances Left: {ercnt}")
+    if ercnt == 0:
+      print("Game Over")
+      compl = True
+      break
+    if numchk == 0:
+      print("You Won")
+      compl = True
+      break      
+  
+
+
+
+      
