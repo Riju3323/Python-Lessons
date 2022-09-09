dc5d9221ccebbf61cba2345463b7550efe1b21ca
Riju <87523667+Riju3323@users.noreply.github.com>
2022-09-09 21:23:05 +0530

    Updated Data, Will make main Code Later

--- Higher Lower Game/CelebrityData.py
+++ Higher Lower Game/CelebrityData.py
@@ -1,1 +1,56 @@
-o
+celebdat = [
+    {
+        'name': 'Instagram',
+        'follower_count': 545,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 477,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 368,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 358,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 344,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 335,
+        'description': 'Actor and Wrestler',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 329,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kadarshian',
+        'follower_count': 329,
+        'description': 'Model',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Beyonce',
+        'follower_count': 274,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    }
+]
