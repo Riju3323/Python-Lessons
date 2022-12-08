3cdc1975b9d70084565c428ea5317407adb71b8c
Riju <87523667+Riju3323@users.noreply.github.com>
2022-12-08 13:43:41 +0530

    Completed

--- Quiz Game/main.py
+++ Quiz Game/main.py
@@ -11,4 +11,9 @@ for i in question_data:
   question_bank.append(new_question)
   
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+  quiz.next_question()
+  
+print("You've completed the Quiz:")
+print(f"Your Final Score is: {quiz.score}/{quiz.question_number} \n")
--- Quiz Game/quiz_brain.py
+++ Quiz Game/quiz_brain.py
@@ -2,14 +2,22 @@ class QuizBrain:
   def __init__(self, q_list):
     self.question_number = 0
     self.question_list = q_list
+    self.score = 0
     
   def next_question(self):
     current_question = self.question_list[self.question_number]
     self.question_number += 1
     ans = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+    self.check_answer(ans, current_question.answer)
   
   def still_has_questions(self):
-    if self.question_number < len(self.question_list):
-      return True
+    return self.question_number < len(self.question_list)
+  
+  def check_answer(self, ans, correct_answer):
+    if ans.lower() == correct_answer.lower():
+      print("You got it right!!")
+      self.score += 1
     else:
-      return False
+      print("You got it wrong!!")
+    print(f"Your Score: {self.score}/{self.question_number} \n")
+      
